c53b42d31e960ebc7b6205291d0bd9520e121810
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:58:31 +0200

    Create bit_set_on.java

---
+++ bit_set_on.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        int res = n | (1 << k);
+        System.out.println(res);
+    }
+}
