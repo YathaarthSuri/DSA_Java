e7fcd623737f487bd41fc49938687b19a9a7c1e1
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:55:56 +0200

    Create print_zigzag.java

---
+++ print_zigzag.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        int[][] arr = new int[row][col];
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for(int i = 0; i < row; i++) {
+            if(i % 2 == 0) {
+                for(int j = 0; j < col; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            } else {
+                for(int j = col - 1; j >= 0; j--) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
