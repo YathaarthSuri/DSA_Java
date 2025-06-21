e06453a950a908b5842118c3d8d1a3140f2f9b4f
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:41:22 +0200

    Create pass_or_file.java

---
+++ pass_or_file.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
+      Scanner scn = new Scanner(System.in);
+      
+      int n = scn.nextInt();
+      
+      if(n >= 35){
+        System.out.println("pass");
+      }
+      else {
+        System.out.println("fail");
+      }
+      
+    }
+}
