54c6cd24a413d8ba38190e1e34fc75bc1b4d8f1a
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:41:51 +0200

    Create odd_or_even.java

---
+++ odd_or_even.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
+      Scanner scn = new Scanner(System.in);
+      
+      int n = scn.nextInt();
+      
+      if(n % 2 == 0){
+        System.out.println("even");
+      }
+      else {
+        System.out.println("odd");
+      }
+      
+    }
+}
