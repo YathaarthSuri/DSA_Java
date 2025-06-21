423e8825052db151602fefc1d6ea6af2e232964f
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:56:27 +0200

    Create reverse_string.java

---
+++ reverse_string.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        char[] arr = str.toCharArray();
+        int start = 0;
+        int end = str.length() - 1;
+        while(start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        String ans = "".valueOf(arr);
+        System.out.println(ans);
+    }
+}
