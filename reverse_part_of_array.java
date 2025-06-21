2e7b1b88063cb6635ca5e3cf327cfefeb2fcdcab
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:52:33 +0200

    Create reverse_part_of_array.java

---
+++ reverse_part_of_array.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int[] arr = new int[n];
+      for(int i = 0; i < n; i++) {
+        arr[i] = scn.nextInt();
+      }
+      int s = scn.nextInt();
+      int e = scn.nextInt();
+      
+      reverse(arr, s, e);
+      
+      for(int i = 0; i < n; i++) {
+        System.out.print(arr[i]+" ");
+      }
+    }
+  
+    public static void reverse(int[] arr, int idx1, int idx2) {
+      while(idx1 < idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+        idx1++;
+        idx2--;
+      }
+    }
+}
