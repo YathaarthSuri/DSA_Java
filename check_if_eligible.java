c33fb2bb2f2fef6c83c2c81991614e51cb5a331a
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:39:09 +0200

    Create check_if_eligible.java

---
+++ check_if_eligible.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      
+      if(n >= 18){
+        System.out.println("eligible");
+      }
+      else {
+        System.out.println("not eligible");
+      }
+      
+    }
+}
