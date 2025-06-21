8a6fad8cd6c76b4589a078d0ae7df709c81a6e7f
Yathaarth Suri <yathaarthsuri@gmail.com>
2025-06-21 16:42:26 +0200

    Create divisible_by_2_or_3.java

---
+++ divisible_by_2_or_3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
+      Scanner scn = new Scanner(System.in);
+      
+      int n = scn.nextInt();
+      
+      if(n % 2 == 0 || n % 3 == 0){
+        System.out.println("divisible");
+      }
+      else {
+        System.out.println("not divisible");
+      }
+      
+    }
+}
